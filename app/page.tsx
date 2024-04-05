c4caa7ea2ef5b0f2ac3cb90a7067efeb8b5aaed7
montycode <alfredo.montoya@cesunbc.edu.mx>
2024-04-05 13:14:18 -0700

    Import uploadthing & shadcn configs

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Eviden - Events</h1>
       <Button>Click me</Button>
     </main>
   );
